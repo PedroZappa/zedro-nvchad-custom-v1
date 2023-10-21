8f446cbee77219664027d2e4e89b6c311c135b91
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-21 18:13:51 +0100

    Neorg Lazy loaded

--- plugins.lua
+++ plugins.lua
@@ -135,6 +135,26 @@ local plugins = {
 		run = function() vim.fn['mkdp#util#install']() end,
 		event = "VeryLazy",
 	},
+	{
+		"nvim-neorg/neorg",
+		build = ":Neorg sync-parsers",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("neorg").setup {
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = "~/notes",
+							},
+						},
+					},
+				},
+			}
+		end,
+	},
 
 	-- Config overrides
 	{
