44eec4f93359cb871f9ba4c448da769fd5486f0c
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-29 18:07:59 +0000

    removed spelling autocommand for debugging

--- init.lua
+++ init.lua
@@ -52,13 +52,6 @@ opt.showmode = true
 
 -- Spelling
 --- Enable spell check for .md and .txt files
-autocmd("BufRead,BufNewFile", {
-	pattern = { "*.md", "*.txt", "*.norg" },
-	callback = function()
-		vim.opt_local.spell = true
-		vim.opt_local.spelllang = "en"
-	end,
-})
 
 -- Time
 vim.opt.updatetime = 50
